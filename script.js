d1427502ef8624535e31e829f5c42f2c7eead524
eudys1 <eudysrosario01@gmail.com>
2022-02-22 20:04:49 +0100

    style cards almost done

--- script.js
+++ script.js
@@ -56,32 +56,36 @@ function DibujaTablero() {
         
         //un bucle para las filas que de nCartas/4 vueltas
         for (let i = 1; i <=nCartas/4; i++) {
-            let row, col, front, card, img, back;
+            let row, col, front, card, imgFront, imgBack, back;
             
             row = document.createElement('div'); //creo fila
-            row.className = 'row';
+            row.className = 'row g-0';
         
             for (let i = 1; i<= 4; i++) { //creo columna y lo que lleva dentro
             
                 col = document.createElement('div');
-                col.className = 'col';
+                col.className = 'col-auto m-2';
              
                 front = document.createElement('div');
                 front.className = 'front';
         
                 card = document.createElement('div');
-                card.className = 'card bg-secondary';
+                card.className = 'card bg-transparent border-0';
         
-                img = new Image(190,245);
-                img.className = 'image';
+                imgBack = new Image(190,245);
+                imgBack.className = 'imageBack ';
+
+                imgFront = new Image(190,245);
+                imgFront.className = 'imageFront ';
     
                 back = document.createElement('div');
                 back.className = 'back';
                 
-                card.appendChild(img);
-                front.appendChild(card);
-                col.appendChild(front);
-                col.appendChild(back);
+                front.appendChild(imgFront);
+                back.appendChild(imgBack);
+                card.appendChild(front);
+                card.appendChild(back);
+                col.appendChild(card);
                 row.appendChild(col);
             }
         
@@ -89,19 +93,28 @@ function DibujaTablero() {
         
         }
     
-        //añade las imagenes al elemento img
-        imagenes = document.getElementsByClassName("image");
+        //añade las imagenes ocultas de la carta
+        imagenesBack = document.getElementsByClassName("imageBack");
         
-        for (let i = 0; i < imagenes.length; i++) {
-            imagenes[i].src = `./img/${cartas[i]}.png`;
+        for (let i = 0; i < imagenesBack.length; i++) {
+            imagenesBack[i].src = `./img/${cartas[i]}.png`;
+        }
+
+        //añade img parte de a tras de la carta
+        imagenesFront = document.getElementsByClassName("imageFront");
+        
+        for (let i = 0; i < imagenesFront.length; i++) {
+            imagenesFront[i].src = `./img/backCard.png`;
         }
 
     }
 
 
-
+    $(".card").flip({
+        trigger: 'click'
+    });
+    
 }
-
 
 
 
